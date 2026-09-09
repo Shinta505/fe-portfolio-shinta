ceba7d2aa2b148dc840e6385e8b2fdedff184c2c
Shinta Nursobah Chairani <shinta.nur505@gmail.com>
2026-09-10 00:42:26 +0700

    Adjust z-index of Navbar component

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -30,7 +30,7 @@ const Navbar = () => {
 
   return (
     <header
-      className={`fixed top-0 left-0 right-0 z-50 transition-all duration-300 ${isScrolled
+      className={`fixed top-0 left-0 right-0 z-45 transition-all duration-300 ${isScrolled
           ? 'bg-bgMain/80 backdrop-blur-md border-b border-borderMuted py-3 shadow-lg'
           : 'bg-transparent py-5'
         }`}
